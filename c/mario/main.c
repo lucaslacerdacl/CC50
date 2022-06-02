405f2f2fe898d53616f635a32db14d3935a86ebf
Lacerda <lucas.lacerda@u4crypto.com>
2022-06-01 23:33:08 -0300

    feat: Mario challange

--- c/mario/main.c
+++ c/mario/main.c
@@ -1,8 +1,37 @@
-#include <stdio.h>
 #include <cs50.h>
+#include <stdio.h>
 
-int main(void) {
-	string name = "Lucas";
+void printSharp(int pyramidIndex)
+{
+  for (int sharpQuantity = pyramidIndex + 1; sharpQuantity > 0; sharpQuantity--)
+  {
+    printf("#");
+  }
+}
 
-	printf("Hello, %s!\n", name);
+void printEmptySpace(int pyramidIndex, int size)
+{
+  for (int emptySpaceQuantity = size - pyramidIndex; emptySpaceQuantity > 0; emptySpaceQuantity--)
+  {
+    printf(" ");
+  }
+}
+
+int askPyramidSize()
+{
+  int size = get_int("Tamanho: ");
+
+  return size;
+}
+
+int main(void)
+{
+  int size = askPyramidSize();
+
+  for (int pyramidIndex = 0; pyramidIndex < size; pyramidIndex++)
+  {
+    printEmptySpace(pyramidIndex, size);
+    printSharp(pyramidIndex);
+    printf("\n");
+  }
 }
